0ef7854301a78178c3416a81c998cd591964dce8
Angelo Santos <gabrielange883@gmail.com>
2024-07-15 13:25:10 -0300

    authenticationServicee #jwt configurations

--- SivirCoffee/SivirCoffee.AuthenticationService/obj/Debug/net8.0/SivirCoffee.AuthenticationService.AssemblyInfo.cs
+++ SivirCoffee/SivirCoffee.AuthenticationService/obj/Debug/net8.0/SivirCoffee.AuthenticationService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SivirCoffee.AuthenticationService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ce498f647332e963c32d9a3adc9640fd0ca9a41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SivirCoffee.AuthenticationService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SivirCoffee.AuthenticationService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
