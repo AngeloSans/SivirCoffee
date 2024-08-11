4499bd1535ac1446f5ae22dd0f45b1d4f265d3f4
Angelo Santos <gabrielange883@gmail.com>
2024-08-11 12:54:46 -0300

    adding PaymentRepository

--- SivirCoffee/SivirCoffee.PaymentService/Infraestrucuture/Repository/PaymentRepository.cs
+++ SivirCoffee/SivirCoffee.PaymentService/Infraestrucuture/Repository/PaymentRepository.cs
@@ -1,6 +1,31 @@
-﻿namespace SivirCoffee.PaymentService.Repository;
+﻿using SivirCoffee.PaymentService.Entities;
 
-public class PaymentRepository
+namespace SivirCoffee.PaymentService.Repository;
+
+public class PaymentRepository : IPaymentRepository
 {
-    
+    public Task<IEnumerable<TypePayment>> GetPaymentMethods()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task MakePayment(Payment payment)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task CancelPayment(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task getPayment(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Payment>> getAllPayment()
+    {
+        throw new NotImplementedException();
+    }
 }
