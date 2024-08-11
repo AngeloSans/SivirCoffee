c20f6bc0851d40835a88bd23ce0e70a12ac651af
Angelo Santos <gabrielange883@gmail.com>
2024-08-11 12:53:43 -0300

    adding paymentcontroller

---
+++ SivirCoffee/SivirCoffee.PaymentService/Application/Controllers/PaymentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SivirCoffee.PaymentService.Application.Controllers;
+
+public class PaymentController : Controller
+{
+    
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
